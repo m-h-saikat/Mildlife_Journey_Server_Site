f7a02f3a6ee2ae8298738bed6e2ef2bb7a09a011
Mehedi Hasan Saikat <29018897+mehedi-hasan-saikat@users.noreply.github.com>
2022-12-17 23:04:51 +0600

    update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { MongoClient } = require("mongodb");
+const { MongoClient,ServerApiVersion} = require("mongodb");
 const ObjectId = require("mongodb").ObjectId;
 const cors = require("cors");
 require("dotenv").config();
@@ -11,16 +11,18 @@ app.use(express.json());
 
 // const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.od1ig.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.blowuoi.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// const client = new MongoClient(uri, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 
 console.log(uri);
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
     const database = client.db("MildlifeJourney");
     const packagesCollection = database.collection("packages");
     const ordersCollection = database.collection("orders");
